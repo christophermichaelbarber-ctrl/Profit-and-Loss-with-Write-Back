ee16f90b33dcbcb7aea622163121669c10472052
christophermichaelbarber-ctrl <christophermichaelbarber@hotmail.com>
2025-10-20 10:14:45 +0100

    Committing 2 items from workspace e156f214-ab08-49af-9f3c-8e957b9b634a

---
+++ Profit and Loss Write Back SQL Database.SQLDatabase/dbo/Tables/Commentary.sql
@@ -0,0 +1,13 @@
+CREATE TABLE [dbo].[Commentary] (
+    [Level]               NVARCHAR (50) NULL,
+    [Date]                NVARCHAR (50) NULL,
+    [TimeFrame_Key]       INT           NULL,
+    [FxSelected_Key]      INT           NULL,
+    [Comment]             NVARCHAR (50) NULL,
+    [User Principle Name] NVARCHAR (50) NULL,
+    [User Name]           NVARCHAR (50) NULL
+);
+
+
+GO
+
